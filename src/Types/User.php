8ecfd64865c48d9211f324a024b372b403949683
Svale Fossåskaret <svale@3by5.no>
2019-09-11 15:16:28 +0200

    users.photoVolumeId -> users.photoVolumeUid

--- src/Types/User.php
+++ src/Types/User.php
@@ -22,7 +22,7 @@ class User extends Schema {
         $this->addStringField('preferredLocale');
         // $this->addField('status')->type(UsersField::statusEnum())->nonNull();
 
-        $volumeId = Craft::$app->getProjectConfig()->get('users.photoVolumeId');
+        $volumeId = Craft::$app->getProjectConfig()->get('users.photoVolumeUid');
         if ($volumeId) {
             $this->addField('photo')
                 ->type($this->request->volumes()->get($volumeId));
